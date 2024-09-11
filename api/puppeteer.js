a125425e7ab6a3082601a4bc9836ebb30cdace65
samruiz <sam@seo-giant.com>
2024-09-10 23:23:37 -0600

    Update puppeteer.js

--- api/puppeteer.js
+++ api/puppeteer.js
@@ -1,13 +1,17 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
 
 module.exports = async (req, res) => {
-    const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        executablePath: '/usr/bin/chromium-browser'
+    });
     const page = await browser.newPage();
     await page.goto('https://www.google.com');
-    await page.type('input[name=q]', 'site:docs.stripe.com');
+    await page.type('input[name=q]', 'site:example.com');
     await page.keyboard.press('Enter');
     await page.waitForSelector('#result-stats');
     const resultStats = await page.$eval('#result-stats', el => el.innerText);
     await browser.close();
-    res.status(200).send(resultStats);
+    res.send(resultStats);
 };
