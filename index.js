35a2eacc69a7910dead944f54ca3fe5db48ede4b
samruiz <sam@seo-giant.com>
2024-09-11 00:06:25 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const puppeteer = require('puppeteer');
+
+module.exports = async (req, res) => {
+    const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
+    const page = await browser.newPage();
+    await page.goto('https://www.google.com');
+    await page.type('input[name=q]', 'site:example.com');
+    await page.keyboard.press('Enter');
+    await page.waitForSelector('#result-stats');
+    const resultStats = await page.$eval('#result-stats', el => el.innerText);
+    await browser.close();
+    res.send(resultStats);
+};
